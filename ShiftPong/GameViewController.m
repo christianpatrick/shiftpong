72b89c61436ee8cd3d14328605aa4898ea2f0577
Christian Helvin <hi@mynameischristian.com>
2018-10-14 01:40:40 -0500

    updated code and made it nice on modern iPhones

--- ShiftPong/GameViewController.m
+++ ShiftPong/GameViewController.m
@@ -80,12 +80,12 @@
     // Configure the view.
     SKView * skView = (SKView *)self.view;
     if (!skView.scene) {
-        skView.showsFPS = YES;
-        skView.showsNodeCount = YES;
+        // skView.showsFPS = YES;
+        // skView.showsNodeCount = YES;
         
         // Create and configure the scene.
         SKScene * scene = [GameScene sceneWithSize:skView.bounds.size];
-        scene.scaleMode = SKSceneScaleModeAspectFill;
+        // scene.scaleMode = SKSceneScaleModeAspectFill;
         
         // Present the scene.
         [skView presentScene:scene];
